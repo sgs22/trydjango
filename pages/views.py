027006cb67488d88c6dfcfe882f52506ac7d4df4
sgs22 <samuel_smith@outlook.com>
2020-09-13 20:39:02 +0100

    2nd commit

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-	return HttpResponse("<h1>Hello World</h1>") #string of html code
+def home_view(request, *args, **kwargs):
+	#return HttpResponse("<h1>Hello World</h1>") #string of html code
+	return render(request, "home.html", {})
+
+def about_view(request, *args, **kwargs):
+	my_context = {
+		"my_text": "This is about us",
+		"my_number": 123
+		
+	}
+	return render(request, "about.html", my_context)
+
+def contact_view(request, *args, **kwargs):
+	return render(request, "contact.html", {})
